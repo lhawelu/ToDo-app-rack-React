1462494020deccaec5c1d9e256c11f621f5f361e
Larry Hawelu <lhawelu@gmail.com>
2021-04-16 12:42:10 -0700

    commit tree da52a26f7ada2659fe43c1d5ade86b4575fd2a06

--- back-end/app/application.rb
+++ back-end/app/application.rb
@@ -6,11 +6,24 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
     if req.path.match(/test/) 
 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "response success"}.to_json ]]
 
+    elsif req.path.match(/tasks/) && req.get?
+
+      return [200, { 'Content-Type' => 'application/json' }, [ Task.all.map {|m| { "id": m.id, "text": m.text, "category": m.category.name }}.to_json ]]
+    
+    elsif req.path.match(/tasks/) && req.post?
+      data = JSON.parse req.body.read
+      task = Task.create(text: data["text"], category_id: Category.find_by(name: data["category"]).id)
+      res_task = {text: task.text, category: task.category.name}
+      return [200, {"Content-Type" => "application/json"}, [res_task.to_json]]
+    
+    elsif req.path.match(/tasks/) && req.delete?
+     task_id = req.path.split("/").last
+     Task.find(task_id).destroy
+      
     else
       resp.write "Path Not Found"
 
--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -4,10 +4,21 @@ Task.destroy_all
 
 puts "Seeding Categories..."
 
-# create categories
+food = Category.create(name: "Food")
+money = Category.create(name: "Money")
+misc = Category.create(name: "Misc")
+code = Category.create(name: "Code")
 
 puts "Seeding tasks..."
 
-# create tasks
+Task.create(text: "Buy Rice", category_id: food.id)
+Task.create(text: "Save a tenner", category_id: money.id)
+Task.create(text: "Build a todo app", category_id: misc.id)
+Task.create(text: "Build todo API", category_id: misc.id)
+Task.create(text: "Get an ISA", category_id: money.id)
+Task.create(text: "Cook rice", category_id: food.id)
+Task.create(text: "Tidy house", category_id: misc.id)
+Task.create(text: "Code", category_id: misc.id)
+Task.create(text: "Save a tenner", category_id: money.id)
 
 puts "Done!"
